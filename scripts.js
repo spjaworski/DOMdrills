c88cda051173410b78583dfbe1edd84b0a9800a3
Scott Jaworski <scottpjaworski@gmail.com>
2021-09-17 13:14:15 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener(DOMContentLoaded), function() {
+let headContainer= document.createElement('div');
+headContainer.className="header-container";
+headContainer.appendChild(headContainer)
+
+let header1 = document.createElement('h1');
+ let firstHeaderText = document.createTextNode('This is an h1')
+ header1.appendChild(firstHeaderText);
+}
